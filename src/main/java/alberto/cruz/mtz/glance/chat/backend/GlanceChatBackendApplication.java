fa2065001fa2747ef51203810f1cbcbfe95d3a0b
alberto cruz mtz <albertocruz8133@proton.me>
2026-06-04 15:52:12 -0600

    inicializa el proyecto con todas sus dependencias

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/GlanceChatBackendApplication.java
@@ -0,0 +1,13 @@
+package alberto.cruz.mtz.glance.chat.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlanceChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlanceChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alberto/cruz/mtz/glance/chat/backend/GlanceChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package alberto.cruz.mtz.glance.chat.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlanceChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
