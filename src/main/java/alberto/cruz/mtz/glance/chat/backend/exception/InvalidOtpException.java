7dea22c5292a3a75e789cbb237ea1daedf933e01
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-09 17:42:23 -0600

    feat: agregar excepciones personalizadas
    - Agrega excepcion personalizada para correos ya registrados o en uso
    llamada EmailAlreadyInUseException
    - Agrega excepcio personalizada para codigos OTP no validos llamada
    InvalidOtpException

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/exception/InvalidOtpException.java
@@ -0,0 +1,7 @@
+package alberto.cruz.mtz.glance.chat.backend.exception;
+
+public class InvalidOtpException extends RuntimeException {
+    public InvalidOtpException(String message) {
+        super(message);
+    }
+}
