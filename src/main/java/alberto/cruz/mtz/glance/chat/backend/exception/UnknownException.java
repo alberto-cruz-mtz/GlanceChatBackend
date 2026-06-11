f60bc29c100ac4028566125bc5c054eada460ad5
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-10 22:18:44 -0600

    feat: agregar manejador de errores global
    - Agrega manejo para errores de validacion producidos por las
    anotaciones como @NotEmpty, @Email, @Pattern, etc, mediante @Valid
    
    - Agrega excepcion personalizada para errores desconocidas, esto con la
    intencion de evitar usar RuntimeException para todo y tener una
    excepcion ya manejada y lista para reemplazar por una excepcion que
    represente el caso de error de forma mas descriptiva mas adelante
    
    - Agrega manejo para errores de UnknownException, este devuelve un
    mensaje simple mediante el ProblemDetail evitando compartir informacion
    de mas al cliente y manda un mensaje en los logs con el mensaje de error
    real

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/exception/UnknownException.java
@@ -0,0 +1,8 @@
+package alberto.cruz.mtz.glance.chat.backend.exception;
+
+public class UnknownException extends RuntimeException {
+    public UnknownException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,57 @@
+package alberto.cruz.mtz.glance.chat.backend.exception.handler;
+
+import alberto.cruz.mtz.glance.chat.backend.exception.UnknownException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final static String ERROR_URI = "https://example.com/error";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProblemDetail> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST; // 400
+
+        Map<String, String> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (existing, replacement) -> existing));
+
+        String firstErrorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .orElse("validation failed for the request parameters");
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, firstErrorMessage);
+        problemDetail.setTitle("Validation Failed");
+        problemDetail.setProperty("fieldErrors", fieldErrors);
+        problemDetail.setType(URI.create(ERROR_URI + "/validation-failed"));
+
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+
+    public ResponseEntity<ProblemDetail> handleUnknownException(UnknownException ignore) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, "An unexpected error occurred");
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(URI.create(ERROR_URI + "/internal-server-error"));
+
+        log.error("An unexpected error occurred", ignore);
+
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+}
