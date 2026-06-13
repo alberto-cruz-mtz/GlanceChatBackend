3ac8df2e1944ae770fc6729e37ea100ce8175df0
alberto cruz mtz <albertocruz8133@proton.me>
2026-06-12 22:44:53 -0600

    feat: agregar endpoints de tipo patch para actualizar el avatar (img) y el username
    - Agrega endpoints de actualizacion para avatar y username
    - Elimina anotacion @CrossOrigin de la clase
    - Marca el MultipartFile del endpoint de configuracion de perfil a no
    requerido

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/controller/rest/ProfileController.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/controller/rest/ProfileController.java
@@ -5,18 +5,16 @@ import alberto.cruz.mtz.glance.chat.backend.dto.ProfileResponse;
 import alberto.cruz.mtz.glance.chat.backend.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
-@CrossOrigin(value = "*")
 @RestController
 @RequestMapping("/api/profiles")
 @RequiredArgsConstructor
@@ -26,13 +24,25 @@ public class ProfileController {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ProfileResponse> customizeProfile(
-            @RequestPart(name = "avatar") MultipartFile file,
+            @RequestPart(name = "avatar", required = false) MultipartFile file,
             @RequestPart(name = "profile") @Valid ProfileRequest request
     ) {
-        log.info("Username: ", request.username());
         var response = this.profileService.setUpProfile(request.id(), request.username(), file);
         return ResponseEntity.ok(response);
     }
 
+    @PatchMapping(path = "/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> updateAvatar(
+            @RequestPart(name = "avatar") MultipartFile file,
+            @RequestPart(name = "id") String id
+    ) {
+        var avatarUrl = this.profileService.updateAvatarUrl(id, file);
+        return ResponseEntity.ok(avatarUrl);
+    }
 
+    @PatchMapping("/username")
+    public ResponseEntity<Void> updateUsername(@RequestBody @Valid ProfileRequest request) {
+        this.profileService.updateUsername(request.id(), request.username());
+        return ResponseEntity.noContent().build();
+    }
 }
