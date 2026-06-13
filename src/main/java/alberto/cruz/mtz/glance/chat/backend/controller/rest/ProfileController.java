fc3ae132ff41689682cba62f7368dc920c537798
alberto cruz mtz <albertocruz8133@proton.me>
2026-06-12 22:02:17 -0600

    feat: agregar controlador ProfileController con un unico endpoint para configurar nombre de usuario y imagen para el avatar, y lo recibe mendiante MultiPartFile

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/controller/rest/ProfileController.java
@@ -0,0 +1,38 @@
+package alberto.cruz.mtz.glance.chat.backend.controller.rest;
+
+import alberto.cruz.mtz.glance.chat.backend.dto.ProfileRequest;
+import alberto.cruz.mtz.glance.chat.backend.dto.ProfileResponse;
+import alberto.cruz.mtz.glance.chat.backend.service.ProfileService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Slf4j
+@CrossOrigin(value = "*")
+@RestController
+@RequestMapping("/api/profiles")
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProfileResponse> customizeProfile(
+            @RequestPart(name = "avatar") MultipartFile file,
+            @RequestPart(name = "profile") @Valid ProfileRequest request
+    ) {
+        log.info("Username: ", request.username());
+        var response = this.profileService.setUpProfile(request.id(), request.username(), file);
+        return ResponseEntity.ok(response);
+    }
+
+
+}
