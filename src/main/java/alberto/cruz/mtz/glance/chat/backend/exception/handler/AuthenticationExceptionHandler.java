5af689115b7d9234a17a451c8a9906542b935c40
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 15:58:49 -0600

    feat: agregar nuevo manejador para excepciones de autenticacion UsernameNotFound y BadCredentials producidos por el bean AuthenticationManager

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/exception/handler/AuthenticationExceptionHandler.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/exception/handler/AuthenticationExceptionHandler.java
@@ -1,11 +1,14 @@
 package alberto.cruz.mtz.glance.chat.backend.exception.handler;
 
-import alberto.cruz.mtz.glance.chat.backend.exception.EmailAlreadyInUseException;
+import alberto.cruz.mtz.glance.chat.backend.exception.UsernameAlreadyInUseException;
 import alberto.cruz.mtz.glance.chat.backend.exception.InvalidOtpException;
 import alberto.cruz.mtz.glance.chat.backend.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,7 +20,7 @@ public class AuthenticationExceptionHandler {
     private final static String ERROR_URL = "https://example.com/error";
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<ProblemDetail> handleUserNotFound(UserNotFoundException exception){
+    public ResponseEntity<ProblemDetail> handleUserNotFound(UserNotFoundException exception) {
         HttpStatus status = HttpStatus.NOT_FOUND;
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
@@ -27,8 +30,8 @@ public class AuthenticationExceptionHandler {
         return ResponseEntity.status(status).body(problemDetail);
     }
 
-    @ExceptionHandler(EmailAlreadyInUseException.class)
-    public ResponseEntity<ProblemDetail> handleEmailAlreadyInUseException(EmailAlreadyInUseException exception) {
+    @ExceptionHandler(UsernameAlreadyInUseException.class)
+    public ResponseEntity<ProblemDetail> handleEmailAlreadyInUseException(UsernameAlreadyInUseException exception) {
         HttpStatus status = HttpStatus.CONFLICT;
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
@@ -39,12 +42,24 @@ public class AuthenticationExceptionHandler {
     }
 
     @ExceptionHandler(InvalidOtpException.class)
-    public ResponseEntity<ProblemDetail> handleInvalidOtp(InvalidOtpException exception){
+    public ResponseEntity<ProblemDetail> handleInvalidOtp(InvalidOtpException exception) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
         problemDetail.setTitle("Invalid OTP Code");
         problemDetail.setType(URI.create(ERROR_URL + "/authentication/invalid-otp-code"));
+
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
+    public ResponseEntity<ProblemDetail> handleAuthentication(AuthenticationException exception) {
+        HttpStatus status = HttpStatus.FORBIDDEN;
+        String message = "Credentials incorrect, Please verify your credentials";
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, message);
+        problemDetail.setType(URI.create(ERROR_URL + "/authentication"));
+        problemDetail.setTitle("Authentication failed");
 
         return ResponseEntity.status(status).body(problemDetail);
     }
