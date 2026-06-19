21f28e9fe9267f2aa2c1d12e59deaa67acac110a
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 15:53:51 -0600

    feat: agregar configuracion para spring security|
    agrega beans de configuracion basica para spring security
    
    - Agrega SecurityFilterChain con CSRF desactivado, CORS desactivados de
    momento en la etapa de desarrollo, cambia el tipo de sesion por una
    sesion STATELESS, deja publicos endpoints de autenticacion basica, marca
    endpoints de /2fa con autenticacion y agrega el filtro de autenticacion
    de JWT
    
    - Agrega Password encoder con implementacion de BCryptPasswordEncoder
    
    - Agrega AuthenticationProvider con implementacion de
    DaoAuthenticationProvider
    
    - Agrega AuthenticationManager que recupera la informacion de la
    configuracion actual

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package alberto.cruz.mtz.glance.chat.backend.configuration.security;
+
+import alberto.cruz.mtz.glance.chat.backend.configuration.security.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationFilter authorizationFilter) {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                //TODO: Volver activar la configuración de CORS
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.POST, "/api/auth/login", "/api/auth/login/2fa", "/api/auth/signup").permitAll();
+                    http.requestMatchers(HttpMethod.POST, "/api/auth/2fa/**").authenticated();
+                    http.anyRequest().denyAll();
+                })
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(this.passwordEncoder());
+        return provider;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
