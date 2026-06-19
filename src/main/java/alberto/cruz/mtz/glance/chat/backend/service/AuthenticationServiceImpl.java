b2de09b2306c61648444f92f42bc39914df0888d
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 16:13:20 -0600

    feat: eliminar los metodos anteriores de la interfaz AuthenticationService y agrega implementacion para los nuevos
    - Implementa los nuevos metodos de la interfaz
    - Agrega nuevas dependencias al servicio como PasswordEncoder,
    AuthenticationManager y ToptAuthenticator

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/service/AuthenticationServiceImpl.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/service/AuthenticationServiceImpl.java
@@ -1,22 +1,24 @@
 package alberto.cruz.mtz.glance.chat.backend.service;
 
+import alberto.cruz.mtz.glance.chat.backend.dto.AccessTokenResponse;
 import alberto.cruz.mtz.glance.chat.backend.dto.AuthenticationResponse;
-import alberto.cruz.mtz.glance.chat.backend.dto.OtpCode;
-import alberto.cruz.mtz.glance.chat.backend.exception.EmailAlreadyInUseException;
-import alberto.cruz.mtz.glance.chat.backend.exception.InvalidOtpException;
+import alberto.cruz.mtz.glance.chat.backend.exception.InvalidJwtException;
+import alberto.cruz.mtz.glance.chat.backend.exception.UnknownException;
 import alberto.cruz.mtz.glance.chat.backend.exception.UserNotFoundException;
+import alberto.cruz.mtz.glance.chat.backend.exception.UsernameAlreadyInUseException;
 import alberto.cruz.mtz.glance.chat.backend.model.User;
 import alberto.cruz.mtz.glance.chat.backend.repository.UserRepository;
-import alberto.cruz.mtz.glance.chat.backend.util.EmailSender;
 import alberto.cruz.mtz.glance.chat.backend.util.JwtUtil;
 import alberto.cruz.mtz.glance.chat.backend.util.PublicIdGenerator;
+import alberto.cruz.mtz.glance.chat.backend.util.TotpAuthenticator;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collections;
 import java.util.UUID;
 
 @Service
@@ -24,95 +26,116 @@ import java.util.UUID;
 public class AuthenticationServiceImpl implements AuthenticationService {
 
     private final UserRepository userRepository;
-    private final EmailSender emailSender;
     private final JwtUtil jwtUtil;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final TotpAuthenticator totpAuthenticator;
 
-    private final Map<String, OtpCode> otpCodeCache = new HashMap<>();
-    private static final int OTP_CODE_EXPIRATION_IN_SECONDS = 300;
-
-    @Override
-    public AuthenticationResponse loginWithOtpCode(String email, String otpCode) {
-        var user = this.findUserByEmail(email);
-        this.validateOtpCode(email, otpCode);
-
-        String accessToken = jwtUtil.generateToken(user.getDisplayName(), user.getId(), UUID.randomUUID().toString());
-        String publicId = PublicIdGenerator.formatPublicIdForDisplay(user.getPublicId());
-
-        return AuthenticationResponse.create(accessToken, publicId, user);
-    }
 
     @Override
-    public void sendAuthenticationOtpCode(String email) {
-        this.verifyUserExists(email);
-        var otpCode = this.generateOtpCode(email);
+    public AuthenticationResponse register(String username, String password) {
+        this.validateIfUsernameAlreadyInUse(username);
 
-        this.otpCodeCache.put(email, otpCode);
-        emailSender.sendEmailWithOtpCode(email, otpCode.value());
-    }
+        String publicId = this.generatePublicId();
+        String passwordHash = passwordEncoder.encode(password);
 
-    @Override
-    public void sendRegistrationOtpCode(String email) {
-        this.verifyEmailNotRegistered(email);
-        var otpCode = this.generateOtpCode(email);
-        this.otpCodeCache.put(email, otpCode);
-
-        emailSender.sendEmailWithOtpCode(email, otpCode.value());
-    }
-
-    @Override
-    public AuthenticationResponse registerWithOtpCode(String email, String otpCode) {
-        this.verifyEmailNotRegistered(email);
-        this.validateOtpCode(email, otpCode);
-
-        String publicId = this.generateUniquePublicId();
-
-        User user = User.create(email, publicId);
+        User user = User.create(username, passwordHash, publicId);
         User createdUser = userRepository.save(user);
 
-        String accessToken = jwtUtil.generateToken(user.getDisplayName(), user.getId(), UUID.randomUUID().toString());
-        String publicIdFormatted = PublicIdGenerator.formatPublicIdForDisplay(createdUser.getPublicId());
-        return AuthenticationResponse.create(accessToken, publicIdFormatted, createdUser);
+        String token = this.generateAuthenticationToken(createdUser);
+
+        return AuthenticationResponse.create(user, token);
     }
 
-    private User findUserByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new UserNotFoundException("User with email address [" + email + "] was not found"));
+    @Override
+    public AuthenticationResponse authenticate(String username, String password) {
+        this.validateCredentials(username, password);
+
+        User user = this.findUserByUsername(username);
+        String token = this.generateAuthenticationToken(user);
+
+        return AuthenticationResponse.create(user, token);
     }
 
-    private OtpCode generateOtpCode(String email) {
-        SecureRandom random = new SecureRandom();
-        int otp = random.nextInt(1000000);
-        String formattedOtpCode = String.format("%06d", otp);
+    @Override
+    public String generateSecretForActive2fa(String username) {
+        String secret = totpAuthenticator.generateSecret();
+        User user = this.findUserByUsername(username);
+        user.setSecret(secret);
+        userRepository.save(user);
 
-        return new OtpCode(formattedOtpCode, email, Instant.now().plusSeconds(OTP_CODE_EXPIRATION_IN_SECONDS));
+        return secret;
     }
 
-    private void validateOtpCode(String email, String otpCode) {
-        var cachedOtpCode = this.otpCodeCache.get(email);
+    @Override
+    public void validateTheFirstOtpCodeAndActive2fa(String username, String code) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found with username: " + username));
 
-        if (cachedOtpCode == null || !cachedOtpCode.otpCodeMatches(otpCode) || cachedOtpCode.isExpired()) {
-            throw new InvalidOtpException("The OTP code is invalid or has expired. Please request a new code.");
+        boolean isValid = totpAuthenticator.verifyCode(user.getSecret(), code);
+
+        if (!isValid) throw new UnknownException("Invalid OTP code, try again");
+
+        user.setEnabled2fa(true);
+        userRepository.save(user);
+    }
+
+    @Override
+    public AccessTokenResponse verifyTotpCodeAndGenerateAccessToken(String temporaryToken, String code) {
+        String username;
+        try {
+            DecodedJWT decodedJWT = jwtUtil.verifyTemporaryToken(temporaryToken);
+            username = jwtUtil.getUsername(decodedJWT);
+        } catch (InvalidJwtException ignore) {
+            throw new UnknownException("Token invalid, please authenticate again");
+        }
+
+        User user = this.findUserByUsername(username);
+
+        if (user.getSecret() == null) {
+            throw new UnknownException("2FA is not active for this user");
+        }
+
+        boolean isValid = totpAuthenticator.verifyCode(user.getSecret(), code);
+
+        if (!isValid) throw new UnknownException("TOTP code is invalid");
+
+        String token = this.generateAccessToken(user.getUsername(), user.getPublicId());
+        return new AccessTokenResponse(token);
+    }
+
+    private String generateAuthenticationToken(User user) {
+        if (user.isEnabled2fa()) return jwtUtil.generateTemporaryToken(user.getUsername(), user.getPublicId());
+
+        //TODO: Cambiar el UUID.randomUUID() por el id de sesion generado por Spring Security
+        return this.generateAccessToken(user.getUsername(), user.getPublicId());
+    }
+
+    private String generateAccessToken(String username, String publicId) {
+        return jwtUtil.generateToken(username, publicId, UUID.randomUUID().toString());
+    }
+
+    private void validateIfUsernameAlreadyInUse(String username) {
+        if (userRepository.existsByUsername(username)) {
+            throw new UsernameAlreadyInUseException("Username is already in use: " + username);
         }
     }
 
-    private void verifyEmailNotRegistered(String email) {
-        if (userRepository.existsByEmail(email)) {
-            throw new EmailAlreadyInUseException("A user with email address [" + email + "] is already registered");
-        }
-    }
-
-    private void verifyUserExists(String email) {
-        if (!userRepository.existsByEmail(email)) {
-            throw new UserNotFoundException("User with email address [" + email + "] does not exist");
-        }
-    }
-
-    private String generateUniquePublicId() {
+    private String generatePublicId() {
         while (true) {
             String publicId = PublicIdGenerator.generatePublicId();
-            if (!userRepository.existsByPublicId(publicId)) {
-                return publicId;
-            }
+            if (!userRepository.existsByPublicId(publicId)) return publicId;
         }
+    }
+
+    private User findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("User not found after successful authentication: " + username));
+    }
+
+    private void validateCredentials(String username, String password) {
+        UsernamePasswordAuthenticationToken credentials = new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList());
+        // Lanza un AuthenticationException si las credenciales no son válidas
+        authenticationManager.authenticate(credentials);
     }
 }
