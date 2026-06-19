769bcaf85f6047c78355ce5fbfb9b3f2d544774b
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 16:00:14 -0600

    refactor: actualizar los nombres de los metodos de busqueda para que coincidan con nuevo nombre de la propiedad

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/repository/UserRepository.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/repository/UserRepository.java
@@ -7,9 +7,9 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
-    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 
-    boolean existsByEmail(String email);
+    boolean existsByUsername(String email);
 
     boolean existsByPublicId(String publicId);
 }
