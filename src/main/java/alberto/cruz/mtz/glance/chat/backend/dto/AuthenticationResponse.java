915249277e4993d4276852edf08212bb76270c7b
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 16:10:15 -0600

    feat: agrega nuevos campos al DTO AuthenticationResponse
    - Agrega campos accessToken y requiredAuthenticationFor2FA
    - Agrega los nuevos campos en el metodo de creacion estatico del record

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationResponse.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationResponse.java
@@ -1,19 +1,28 @@
 package alberto.cruz.mtz.glance.chat.backend.dto;
 
 import alberto.cruz.mtz.glance.chat.backend.model.User;
-
-import java.time.Instant;
+import alberto.cruz.mtz.glance.chat.backend.util.PublicIdGenerator;
 
 public record AuthenticationResponse(
-        String accessToken,
         String id,
         String publicId,
+        String accessToken,
         String avatar,
         String username,
-        boolean hasSetUpProfile
+        boolean hasSetUpProfile,
+        boolean requiredAuthenticateFor2FA
 ) {
 
-    public static AuthenticationResponse create(String token, String publicId, User user) {
-        return new AuthenticationResponse(token, user.getId(), publicId, user.getAvatarUrl(), user.getDisplayName(), user.isHasSetUpProfile());
+    public static AuthenticationResponse create(User user, String token) {
+        String formattedPublicId = PublicIdGenerator.formatPublicIdForDisplay(user.getPublicId());
+        return new AuthenticationResponse(
+                user.getId(),
+                formattedPublicId,
+                token,
+                user.getAvatarUrl(),
+                user.getUsername(),
+                user.isHasSetUpProfile(),
+                user.isEnabled2fa()
+        );
     }
 }
