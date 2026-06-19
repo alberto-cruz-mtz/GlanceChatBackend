93f9e2041d4d0ba89e1ae819ccf3d3cb52a63929
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 16:15:06 -0600

    feat: eliminar los endponts anteriores que solicitaban codigo TOTP y agrega endpoints tradicionales /register y /login con autenticion y activacion de 2FA

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/controller/rest/AuthenticationController.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/controller/rest/AuthenticationController.java
@@ -1,12 +1,17 @@
 package alberto.cruz.mtz.glance.chat.backend.controller.rest;
 
-import alberto.cruz.mtz.glance.chat.backend.dto.AuthenticationResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.AccessTokenResponse;
 import alberto.cruz.mtz.glance.chat.backend.dto.AuthenticationRequest;
-import alberto.cruz.mtz.glance.chat.backend.dto.AuthenticationWithOtpRequest;
+import alberto.cruz.mtz.glance.chat.backend.dto.AuthenticationResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.Secret2faResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.TotpCode;
+import alberto.cruz.mtz.glance.chat.backend.dto.TotpCodeWithTemporaryTokenRequest;
 import alberto.cruz.mtz.glance.chat.backend.service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,28 +24,35 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/signup/request-code")
-    public ResponseEntity<Void> requestCodeForSignup(@RequestBody @Valid AuthenticationRequest request) {
-        authenticationService.sendRegistrationOtpCode(request.email());
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/login/request-code")
-    public ResponseEntity<Void> requestCodeForLogin(@RequestBody @Valid AuthenticationRequest request){
-        authenticationService.sendAuthenticationOtpCode(request.email());
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/login/verify")
-    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody @Valid AuthenticationWithOtpRequest request){
-        var response = authenticationService.loginWithOtpCode(request.email(), request.OTP());
+    @PostMapping("/signup")
+    public ResponseEntity<AuthenticationResponse> registerNewUser(@RequestBody @Valid AuthenticationRequest request) {
+        var response = this.authenticationService.register(request.username(), request.password());
         return ResponseEntity.ok(response);
     }
 
-
-    @PostMapping("/signup/verify")
-    public ResponseEntity<AuthenticationResponse> register(@RequestBody @Valid AuthenticationWithOtpRequest request){
-        var response = authenticationService.registerWithOtpCode(request.email(), request.OTP());
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticateUser(@RequestBody @Valid AuthenticationRequest request) {
+        var response = this.authenticationService.authenticate(request.username(), request.password());
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/login/2fa")
+    public ResponseEntity<AccessTokenResponse> validateTotpCode(@RequestBody @Valid TotpCodeWithTemporaryTokenRequest request) {
+        var response = this.authenticationService.verifyTotpCodeAndGenerateAccessToken(request.temporaryToken(), request.totpCode());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/2fa/generate")
+    public ResponseEntity<Secret2faResponse> generateSecret2fa(Authentication authentication) {
+        IO.println(authentication);
+        String secret = this.authenticationService.generateSecretForActive2fa(authentication.getName());
+        return ResponseEntity.ok(new Secret2faResponse(secret));
+    }
+
+    @PostMapping("/2fa/enable")
+    public ResponseEntity<Void> active2fa(Authentication authentication, @RequestBody @Valid TotpCode totpCode) {
+        this.authenticationService.validateTheFirstOtpCodeAndActive2fa(authentication.getName(), totpCode.value());
+        return ResponseEntity.noContent().build();
+    }
+
 }
