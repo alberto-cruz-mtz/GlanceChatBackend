d3e000f8a43b042686cd9840e16e652c4a479cce
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-19 16:09:54 -0600

    feat: actualiza campo email del DTO AuthenticationRequest y agrega nueva propiedad
    - Elimina el campo Email
    - Agrega campo username y agrega validacion para valores vacios
    - Agrega campo password con validaciones para valor vacio o cadena
    vacia, formato de contraseña para contener mayusculas, minusculas,
    numeros y simbolos

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationRequest.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationRequest.java
@@ -1,11 +1,15 @@
 package alberto.cruz.mtz.glance.chat.backend.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record AuthenticationRequest(
-        @Email(message = "The email must be a valid email address")
-        @NotEmpty(message = "The email must not be empty")
-        String email
+        @NotEmpty(message = "The username is required")
+        String username,
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "The password must be at least 8 characters long and contain at least one letter and one number")
+        @NotEmpty(message = "The password is required")
+        @Size(min = 8, message = "The password must be at least 8 characters long")
+        String password
 ) {
 }
