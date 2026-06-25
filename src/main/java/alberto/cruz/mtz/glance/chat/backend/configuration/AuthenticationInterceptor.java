06ce80d69c42c6e06da909c70cae1adc9e92f4ad
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-06-25 16:44:39 -0600

    feat: agregar recuperacion del y validacion del JWT durante la conexion con websockes
    Agrega recuperacion del token JWT y valida que aun no haya expirado o
    haya sido corrompido, en caso de ser un token invalido se lanzara un
    MessageDelivaryException con el cual se deniega el acceso al WebSocket

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/configuration/AuthenticationInterceptor.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/configuration/AuthenticationInterceptor.java
@@ -1,10 +1,17 @@
 package alberto.cruz.mtz.glance.chat.backend.configuration;
 
+import alberto.cruz.mtz.glance.chat.backend.exception.InvalidJwtException;
+import alberto.cruz.mtz.glance.chat.backend.exception.UnknownException;
+import alberto.cruz.mtz.glance.chat.backend.util.JwtUtil;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.sun.security.auth.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageDeliveryException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -12,10 +19,13 @@ import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Component;
 
 import java.security.Principal;
-import java.util.UUID;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class AuthenticationInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
 
     @Override
     public @NonNull Message<?> preSend(@NonNull Message<?> message, @NonNull MessageChannel channel) {
@@ -23,14 +33,27 @@ public class AuthenticationInterceptor implements ChannelInterceptor {
 
         assert accessor != null;
         if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            log.info("Iniciando autenticacion para el websocket");
             String authorization = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
+            try {
+                if (authorization != null && authorization.startsWith("Bearer ")) {
+                    String token = authorization.substring(7);
+                    DecodedJWT decodedJWT = jwtUtil.verify(token);
 
-            // TODO: cambiar el valor de recuperación de la cabecera de UUID a un Token
-            if (authorization != null && !authorization.isEmpty()) {
-                UUID uid = UUID.fromString(authorization);
+                    String userId = jwtUtil.getUserId(decodedJWT);
 
-                Principal principal = new UserPrincipal(uid.toString());
-                accessor.setUser(principal);
+                    Principal principal = new UserPrincipal(userId);
+                    accessor.setUser(principal);
+
+                    log.info("User authenticated: {}", principal);
+                } else {
+                    log.error("Authorization header is missing or invalid");
+                    throw new UnknownException("Authorization header is missing or invalid");
+                }
+            } catch (InvalidJwtException invalidJwtException) {
+                throw new MessageDeliveryException("Forbidden: Invalid access token, please provide a valid access token to connect to the WebSocket.");
+            } catch (UnknownException unknownException) {
+                throw new MessageDeliveryException("Forbidden: Authorization header is missing or invalid, please provide a valid access token to connect to the WebSocket.");
             }
         }
 
