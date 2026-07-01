5bc02563b1bfb15c54775f5442d875eca35be725
alberto cruz mtz <albertocruz8133@proton.me>
2026-06-30 19:41:34 -0600

    feat: actualizar el nombre del campo username a displayName en el DTO ProfileRequest

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/ProfileRequest.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/ProfileRequest.java
@@ -1,9 +1,10 @@
 package alberto.cruz.mtz.glance.chat.backend.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 public record ProfileRequest(
-        @NotEmpty(message = "The ID is required") String id,
-        @NotEmpty(message = "The username is required") String username
+        @Size(min = 5, max = 50, message = "The displayName must be between 5 and 50 characters")
+        @NotEmpty(message = "The displayName is required") String displayName
 ) {
 }
