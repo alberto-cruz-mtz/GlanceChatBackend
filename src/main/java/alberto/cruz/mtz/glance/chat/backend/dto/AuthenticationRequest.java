9fe68b39a4861c073ef47ebe40712544796cf5ba
alberto cruz mtz <albertocruz8133@proton.me>
2026-06-30 19:40:23 -0600

    feat: agregar validaciones de formato y tamaño para el username
    - Agrega validacion del formato del username mediante la anotacion
    @Pattern, ahora solo se acepta nombres de usuario con caracteres
    alfanumericos y guiones bajos
    - Agrega validacion de tamaño de caracteres, ahora el nombre de usuario
    debe tener de a 5 a 25 caracteres

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationRequest.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/AuthenticationRequest.java
@@ -5,7 +5,9 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record AuthenticationRequest(
-        @NotEmpty(message = "The username is required")
+        @Pattern(regexp = "^[a-zA-Z0-9_]{3,25}$", message = "The displayName can only contain letters, numbers and underscores")
+        @Size(min = 5, max = 25, message = "The displayName must be between 5 and 25 characters long")
+        @NotEmpty(message = "The displayName is required")
         String username,
         @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "The password must be at least 8 characters long and contain at least one letter and one number")
         @NotEmpty(message = "The password is required")
