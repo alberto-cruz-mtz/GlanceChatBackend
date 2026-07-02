6518d1eacf8e6ebcf7011276bfcc8268f2d6e36e
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-07-02 16:23:39 -0600

    feat: agregar interfaz y clase de servicio para mensajes
    - Agrega interfaz con metodos exponer a controladores
    - Agrega clase de implementacion de interfaz con la logica de proceso y
    dependencias
    - Agrega metodo para guardar mensaje duranta la comunicacion entre
    usuarios con websockets
    - Agrega metodo para devolver mensajes de una conversacion con
    paginacion

---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/service/MessageService.java
@@ -0,0 +1,17 @@
+package alberto.cruz.mtz.glance.chat.backend.service;
+
+import alberto.cruz.mtz.glance.chat.backend.dto.DataResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.MessageResponse;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface MessageService {
+
+    /**
+     * @return the ID of the saved message
+     */
+    String saveMessage(List<String> conversationIds, String senderId, String content);
+
+    DataResponse<MessageResponse> getMessagesByConversationId(Pageable pageable, String conversationId);
+}
---
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/service/MessageServiceImpl.java
@@ -0,0 +1,59 @@
+package alberto.cruz.mtz.glance.chat.backend.service;
+
+import alberto.cruz.mtz.glance.chat.backend.dto.DataResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.PaginationLinks;
+import alberto.cruz.mtz.glance.chat.backend.dto.MessageResponse;
+import alberto.cruz.mtz.glance.chat.backend.dto.Pagination;
+import alberto.cruz.mtz.glance.chat.backend.model.Message;
+import alberto.cruz.mtz.glance.chat.backend.repository.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MessageServiceImpl implements MessageService {
+
+    private final MessageRepository messageRepository;
+
+    @Override
+    public String saveMessage(List<String> conversationIds, String senderId, String content) {
+        Message message = Message.builder()
+                .conversationIds(conversationIds)
+                .senderId(senderId)
+                .content(content)
+                .build();
+
+        Message createdMessage = messageRepository.save(message);
+        return createdMessage.getId();
+    }
+
+    @Override
+    public DataResponse<MessageResponse> getMessagesByConversationId(Pageable pageable, String conversationId) {
+        var messagePage = messageRepository.findByConversationIds(conversationId, pageable);
+
+        int currentPage = messagePage.getNumber();
+        int perPage = messagePage.getSize();
+
+        String prev = messagePage.hasPrevious() ? "http://localhost:8080/api/messages?page=" + (currentPage - 1) + "&size=" + perPage : null;
+        String next = messagePage.hasNext() ? "http://localhost:8080/api/messages?page=" + (currentPage + 1) + "&size=" + perPage : null;
+
+        PaginationLinks links = new PaginationLinks(prev, next);
+
+        Pagination paginationRequest = new Pagination(currentPage, perPage, messagePage.getNumberOfElements(), messagePage.hasNext(), links);
+
+        List<MessageResponse> messages = messagePage
+                .map(message -> new MessageResponse(
+                        message.getId(),
+                        message.getContent(),
+                        conversationId,
+                        message.getSenderId()
+                ))
+                .toList();
+
+        return new DataResponse<>(messages, paginationRequest);
+    }
+}
