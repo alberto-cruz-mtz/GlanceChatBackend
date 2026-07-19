ce22ef3c1406a2b8459caf2f52df6ca9e61598cd
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-07-19 17:28:18 -0600

    feat: agregar el publicId en la informacion del Recipient

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/Recipient.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/dto/Recipient.java
@@ -2,6 +2,7 @@ package alberto.cruz.mtz.glance.chat.backend.dto;
 
 public record Recipient(
         String id,
+        String publicId,
         // el name puede ser el username o displayName dependiendo
         // si el usuario configuro un displayName o no, si no tiene displayName se mostrara el username
         String name,
