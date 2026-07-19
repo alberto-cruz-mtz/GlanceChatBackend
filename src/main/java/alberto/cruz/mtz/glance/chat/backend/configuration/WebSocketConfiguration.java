a023193f9485ab6134f67dc715acf74a7d116668
jose alberto cruz martinez <albertocruz8133@proton.me>
2026-07-19 17:27:16 -0600

    feat: reemplazar el uso de la barra (/) como separador por (.) para las rutas de websockets

--- src/main/java/alberto/cruz/mtz/glance/chat/backend/configuration/WebSocketConfiguration.java
+++ src/main/java/alberto/cruz/mtz/glance/chat/backend/configuration/WebSocketConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -23,6 +24,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setPathMatcher(new AntPathMatcher("."));
         registry.enableSimpleBroker("/queue");
         registry.setApplicationDestinationPrefixes("/app");
         registry.setUserDestinationPrefix("/user");
